1c030c46a243794ecca0895b9f523b8b1cce3a4a
Teo <teov5058@gmail.com>
2024-11-17 11:45:41 +0000

    verificare ip uri cu parametri

--- verif_ip.sh
+++ verif_ip.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-cat /etc/hosts | while read linie
-do
-	if [[ "$linie" =~ ^# ]]; then
-		exit 0
-	fi
-
-	IP=`echo "$linie" | awk '{print $1}'`
-	nume=`echo "$linie" | awk '{print $2}'`
-
-	IP_nslookup=`nslookup $nume 8.8.8.8 | grep 'Address' | tail -n 1 | awk '{print $2}'`
+host=$1
+IP=$2
+DNS=$3
+IP_nslookup=`nslookup "$host" "$DNS" | grep 'Address' | tail -n 1 | awk '{print $2}'`
 
 	if [[ "$IP" != "$IP_nslookup" ]]; then
 		echo "Bogus IP for $nume in /etc/hosts! Wrong ip:$IP Right ip:$IP_nslookup "
